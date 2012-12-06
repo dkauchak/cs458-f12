8cf4e5a67c2bab1c4026410f50d26bf87ab6885d
Colby Horn <chorn@middlebury.edu>
2012-12-06 15:12:41 -0500

    Added command line audio search interface.

--- search/audio/data/AudioReader.java
+++ search/audio/data/AudioReader.java
@@ -3,6 +3,8 @@ package search.audio.data;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.NoSuchElementException;
 
 import javax.sound.sampled.AudioFormat;
@@ -20,6 +22,12 @@ public class AudioReader {
 	public AudioReader(String audioDir) {
 		File tempDir = new File(audioDir);
 		files = tempDir.listFiles();
+		Arrays.sort(files, new Comparator<File>() {
+			@Override
+			public int compare(File f1, File f2) {
+				return f1.getName().compareTo(f2.getName());
+			}
+		});
 		filePtr = 0;
 	}
 
