362238e5d42b61af97a3a227093bfd27552f5bab
Billy (Doo Hyun) Lee <doohyunl@middlebury.edu>
2012-12-21 19:00:27 -0500

    Edited format

--- search/Processor.java
+++ search/Processor.java
@@ -7,7 +7,7 @@ import search.data.WebDocumentReader;
 
 public class Processor {
 
-  WebDocumentReader reader, reader2;
+  	WebDocumentReader reader, reader2;
 	ImprovedTokenizer tokenizer;
 	TokenProcessor processor;
 	Index index;
@@ -19,33 +19,33 @@ public class Processor {
 		
 		tokenizer = new ImprovedTokenizer();
 		processor = new TokenProcessor();				
-		processor.setLowercase(true);
+		processor.setLowercase( true );
 
-		reader = new WebDocumentReader(web_data_file);
-		reader.setTokenizer(tokenizer);
-		reader.setTokenProcessor(processor);
+		reader = new WebDocumentReader( web_data_file );
+		reader.setTokenizer( tokenizer );
+		reader.setTokenProcessor( processor );
 
-		reader2 = new WebDocumentReader(web_data_file);
-		reader2.setTokenizer(tokenizer);
-		reader2.setTokenProcessor(processor);
+		reader2 = new WebDocumentReader( web_data_file );
+		reader2.setTokenizer( tokenizer );
+		reader2.setTokenProcessor( processor );
 
-		index = new Index(reader, Index.TERM_MODIFIER.l, Index.DOC_MODIFIER.t, Index.LENGTH_MODIFIER.n );
+		index = new Index( reader, Index.TERM_MODIFIER.l, Index.DOC_MODIFIER.t, Index.LENGTH_MODIFIER.n );
 		id2web = new Hashtable<Integer, WebResultData>();
 		while( reader2.hasNext() ){
-			WebDocument next = (WebDocument)reader2.next();
-			id2web.put(next.getDocID(), new WebResultData(next.getTitle(), next.getUrl(), next.getText()));
+			WebDocument next = ( WebDocument )reader2.next();
+			id2web.put( next.getDocID(), new WebResultData( next.getTitle(), next.getUrl(), next.getText() ) );
 		}
 	}
 
-	public ArrayList<WebResultData> processResults(String query) {
+	public ArrayList<WebResultData> processResults( String query ) {
 
 		ArrayList<WebResultData> searchResults = new ArrayList<WebResultData>();
-		VectorResult rankedResult = index.rankedQuery(new Query(processor.process(tokenizer.tokenize(query))));
+		VectorResult rankedResult = index.rankedQuery( new Query( processor.process( tokenizer.tokenize( query ) ) ) );
 		int[] IDs = rankedResult.getIDs();
 		double[] scores = rankedResult.getScores();
 
-		for(int i = 0; i < scores.length; i++) {
-			searchResults.add(id2web.get(IDs[i]));	
+		for( int i = 0; i < scores.length; i++ ) {
+			searchResults.add( id2web.get( IDs[i] ) );	
 		}
 		return searchResults;
 	}
